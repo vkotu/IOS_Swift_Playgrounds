90e8b9ba72b8fb55beef8d1d4e559600e2ed1030
venkat <vikkii89@gmail.com>
2016-02-05 22:57:37 -0800

    new playground

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,56 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playgrounds"
+    str = "good bye!"
+var name = "vicky"
+    name = "kotu"
+
+var strn: String = "newString"
+    strn = "updatedString"
+
+var unkownString:String
+    unkownString = "notNow"
+
+var int = 3
+
+3*int
+
+var a:Int = 5
+
+var b = a * 3
+
+var c:Double = 7 / 2
+
+var d = 5 / 2
+
+c * 2
+
+
+var longStr  =   str + " " + name
+
+var newStr = "my son is \(int)"
+
+var a1 = 3.5
+
+var b1 = 4
+
+var c1 = a1 * Double(b1)
+
+var s = "\(a1) * \(b1) = \(c1)"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
